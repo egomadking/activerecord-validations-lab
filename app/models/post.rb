92d66099dd970870bb0fdcfccfe3daf68988143b
Roy Mosby <ego.madking@gmail.com>
2021-04-10 11:32:20 -0400

    commit tree fe26522aa0eaf5865242215dfc74ef0779821198

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :is_clickbaity?
+
+  def is_clickbaity?
+    keywords = /won't believe|secret|top\s*\d+|guess/i
+    unless keywords.match title
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
+
+
